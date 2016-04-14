7e6916cadf5cc35e0031511bf0457f3d699d91c1
lefoy <me@lefoy.net>
2016-04-14 10:08:55 -0400

    Update customGlobalClasses default config

--- dist/jquery.buttonize.js
+++ dist/jquery.buttonize.js
@@ -17,7 +17,7 @@
             clicked: 'is-clicked',
             extern: 'is-external',
             error: 'is-error'
-        }, this.config.customGlobalClasses || {});
+        }, (window.classes !== undefined ? window.classes : this.config.customGlobalClasses) || {});
 
         this.init();
     };
